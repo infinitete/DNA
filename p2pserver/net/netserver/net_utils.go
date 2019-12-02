fc88f2fa2f2c6774798de02bd58b896b2492bed8
田仁山 <tianrenshan@fe-cred.com>
2019-12-02 14:30:13 +0800

    设置TLS通讯配置：InsecureSkipVerify 为 true，即不需要验证主机名

--- p2pserver/net/netserver/net_utils.go
+++ p2pserver/net/netserver/net_utils.go
@@ -90,6 +90,13 @@ func TLSDial(nodeAddr string) (net.Conn, error) {
 	conf := &tls.Config{
 		RootCAs:      clientCertPool,
 		Certificates: []tls.Certificate{cert},
+
+		// InsecureSkipVerify控制客户端是否认证服务端的证书链和主机名。
+		// 如果InsecureSkipVerify为真，TLS连接会接受服务端提供的任何证书和该证书中的任何主机名。
+		// 此时，TLS连接容易遭受中间人攻击，这种设置只应用于测试。
+		//
+		// 重要提示：IDFOR 通过IP通讯，此处可以忽略验证
+		InsecureSkipVerify: true,
 	}
 
 	var dialer net.Dialer
